3d94dd7149207f8442261e6a32e7bc10758a6a93
Preetham Nadig <preethamnadig@yahoo.com>
2015-03-15 03:22:56 +0000

    added privacy to collection removed insecure and autopublish

--- client/parties/parties.controller.js
+++ client/parties/parties.controller.js
@@ -1,7 +1,7 @@
-angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
-  function($scope, $meteor){
+angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '$rootScope',
+  function($scope, $meteor, $rootScope){
 
-    $scope.parties = $meteor.collection(Parties);
+    $scope.parties = $meteor.collection(Parties).subscribe('parties');
     
     $scope.remove = function(party){
   $scope.parties.remove(party);
@@ -12,10 +12,11 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
 };
 
   }])
-.controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
-  function($scope, $stateParams,$meteor){
+.controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor','$rootScope',
+  function($scope, $stateParams,$meteor, $rootScope){
 
-    $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+    $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
     
     $scope.save = function() {
       $scope.party.save().then(function(numberOfDocs){
