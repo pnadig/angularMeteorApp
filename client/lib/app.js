3520ca7ab75be1f75266a87b29485dc05128a4fb
Preetham Nadig <preethamnadig@yahoo.com>
2015-03-15 03:05:25 +0000

    before removing meteor unsecure

---
+++ client/lib/app.js
@@ -0,0 +1,10 @@
+
+    angular.module('socially',['angular-meteor', 'ui.router']);
+  
+  
+  
+    
+
+    
+
+
---
+++ client/parties/parties.controller.js
@@ -0,0 +1,33 @@
+angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
+  function($scope, $meteor){
+
+    $scope.parties = $meteor.collection(Parties);
+    
+    $scope.remove = function(party){
+  $scope.parties.remove(party);
+};
+    
+    $scope.removeAll = function(){
+  $scope.parties.remove();
+};
+
+  }])
+.controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
+  function($scope, $stateParams,$meteor){
+
+    $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+    
+    $scope.save = function() {
+      $scope.party.save().then(function(numberOfDocs){
+        console.log('save success doc affected ', numberOfDocs);
+      }, function(error){
+        console.log('save error', error);
+      });
+    };
+
+$scope.reset = function() {
+  $scope.party.reset();
+};
+
+
+}]);
