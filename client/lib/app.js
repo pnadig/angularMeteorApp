dbbbe3cbf52624c1309a19c59c0ee0d1c7efa089
Preetham Nadig <preetham.nadig@datalynx.sg>
2015-03-15 16:57:49 +0800

    added sorting and pagination

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,14 +1,7 @@
 
-    angular.module('socially',['angular-meteor', 'ui.router']);
-
-    function onReady() {
-  angular.bootstrap(document, ['socially']);
-}
-
-if (Meteor.isCordova)
-  angular.element(document).on("deviceready", onReady);
-else
-  angular.element(document).ready(onReady);
+    angular.module('socially',['angular-meteor', 
+    	'ui.router', 
+    	'angularUtils.directives.dirPagination']);
   
   
   
--- client/parties/parties.controller.js
+++ client/parties/parties.controller.js
@@ -1,7 +1,38 @@
 angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '$rootScope',
   function($scope, $meteor, $rootScope){
 
-    $scope.parties = $meteor.collection(Parties).subscribe('parties');
+    $scope.page = 1;
+$scope.perPage = 3;
+$scope.sort = { name: 1 };
+
+$scope.orderProperty = '1';
+
+    $scope.parties = $meteor.collection(function() {
+  return Parties.find({}, {
+    sort : $scope.getReactively('sort')
+  });
+});
+
+    $scope.$watch('orderProperty', function(){
+  if ($scope.orderProperty)
+    $scope.sort = {name: parseInt($scope.orderProperty)};
+});
+
+    $scope.pageChanged = function(newPage) {
+  $scope.page = newPage;
+};
+
+$meteor.autorun($scope, function() {
+
+  $meteor.subscribe('parties', {
+  limit: parseInt($scope.getReactively('perPage')),
+  skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
+  sort: $scope.getReactively('sort')
+}, $scope.getReactively('search')).then(function(){
+    $scope.partiesCount = $meteor.object(Counts ,'numberOfParties', false);
+  });
+
+});
     
     $scope.remove = function(party){
   $scope.parties.remove(party);
@@ -15,8 +46,23 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '
 .controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor','$rootScope',
   function($scope, $stateParams,$meteor, $rootScope){
 
+
+
+
     $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
     $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+
+    var subscriptionHandle;
+
+$meteor.subscribe('parties').then(function(handle) {
+  subscriptionHandle = handle;
+});
+
+$scope.$on('$destroy', function() {
+  subscriptionHandle.stop();
+});
+
+
     
     $scope.save = function() {
       $scope.party.save().then(function(numberOfDocs){
